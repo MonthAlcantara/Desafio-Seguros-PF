fc77be6c9a658bc6217fe067d0b77f5d1915ff2c
MonthAlcantara <montival_junior@yahoo.com.br>
2025-08-07 22:16:31 -0300

    feat: adiciona projeto

---
+++ src/test/java/io/github/monthalcantara/acme/AcmeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.acme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
